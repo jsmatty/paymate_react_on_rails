5750a308a77e6c6ec4873506761b8cc5ea3a99f1
jsmatty <jscot.matty@gmail.com>
2017-09-17 02:23:29 -0400

    creating nested and independent routes for houses and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  namespace :api do
+    resources :users
+      resources :houses do
+        resources :users
+      end
+    end
+  end
